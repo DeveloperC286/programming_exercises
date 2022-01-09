245c12942780b9fad90e2e0757afda4aec52b28a
DeveloperC <DeveloperC@protonmail.com>
2022-01-09 01:08:10 +0000

    refactor: correct test casing

--- advent_of_code/2021/day2/haskell/tests/Tests.hs
+++ advent_of_code/2021/day2/haskell/tests/Tests.hs
@@ -3,14 +3,14 @@ import Day2
 import System.Exit
 import Test.HUnit
 
-test_calculatePosition :: Test
-test_calculatePosition = TestCase (assertEqual "" 150 (calculatePosition [(Forward, 5), (Down, 5), (Forward, 8), (Up, 3), (Down, 8), (Forward, 2)]))
+testCalculatePosition :: Test
+testCalculatePosition = TestCase (assertEqual "" 150 (calculatePosition [(Forward, 5), (Down, 5), (Forward, 8), (Up, 3), (Down, 8), (Forward, 2)]))
 
-test_calculatePositionWithAim :: Test
-test_calculatePositionWithAim = TestCase (assertEqual "" 900 (calculatePositionWithAim [(Forward, 5), (Down, 5), (Forward, 8), (Up, 3), (Down, 8), (Forward, 2)]))
+testCalculatePositionWithAim :: Test
+testCalculatePositionWithAim = TestCase (assertEqual "" 900 (calculatePositionWithAim [(Forward, 5), (Down, 5), (Forward, 8), (Up, 3), (Down, 8), (Forward, 2)]))
 
 tests :: Test
-tests = TestList [TestLabel "test_calculatePosition" test_calculatePosition, TestLabel "test_calculatePositionWithAim" test_calculatePositionWithAim]
+tests = TestList [TestLabel "testCalculatePosition" testCalculatePosition, TestLabel "testCalculatePositionWithAim" testCalculatePositionWithAim]
 
 main = do
   count <- runTestTT tests
