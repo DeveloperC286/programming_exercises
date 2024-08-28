e0d941f420cbad29b9f2fedd7406ca5439137851
DeveloperC <DeveloperC@protonmail.com>
2024-08-28 23:43:09 +0100

    refactor: replacing unwraps with iter

--- largest_number/rust/src/lib.rs
+++ largest_number/rust/src/lib.rs
@@ -2,20 +2,25 @@
 extern crate proptest;
 
 pub fn largest_number(searching: &[i32]) -> Option<i32> {
-    match searching.len() {
-        0 => None,
-        1 => Some(*searching.get(0).unwrap()),
-        _ => {
-            let top = *searching.get(0).unwrap();
-            let largest_in_sub_vector = largest_number(&searching[1..]).unwrap();
-
-            if top > largest_in_sub_vector {
-                Some(top)
-            } else {
-                Some(largest_in_sub_vector)
-            }
+    fn internal_largest_number<'a>(
+        mut searching: impl std::iter::Iterator<Item = &'a i32>,
+    ) -> Option<i32> {
+        match searching.next() {
+            None => None,
+            Some(head) => match internal_largest_number(searching) {
+                None => Some(*head),
+                Some(next_largest) => {
+                    if *head > next_largest {
+                        Some(*head)
+                    } else {
+                        Some(next_largest)
+                    }
+                }
+            },
         }
     }
+
+    internal_largest_number(searching.iter())
 }
 
 #[cfg(test)]
